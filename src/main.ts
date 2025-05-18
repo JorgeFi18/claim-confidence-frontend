007b252d4e05b7c090fc36f0e1a9b2d8fc954a68
JorgeFi18 <jorgefigueroa1897@gmail.com>
2025-05-17 21:56:38 -0600

    Add prod url

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+// Mostrar la configuración actual
+console.log('Environment:', environment);
+console.log('API URL:', environment.apiUrl);
+console.log('Production mode:', environment.production);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
